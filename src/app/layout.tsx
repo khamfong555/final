36a2182fa65031e8936f59aa6ae867acd7e7f2eb
Lattaphone <Aiyphimphisan@gmail.com>
2025-03-04 11:07:07 +0700

    git

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Component from "./component/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
+          <Component/>
         {children}
       </body>
     </html>
--- src/app/one/page.tsx
+++ src/app/one/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import Component from "./component/page";
-
 
 export default function Home() {
   return (
